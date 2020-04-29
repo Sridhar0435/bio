ea7bb4c5cb55ecd14014443e8f1553a3c067460d
Sridhar0435 <ssri668@gmail.com>
2020-04-29 22:45:47 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 import './App.css';
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header"
 import Main from "./components/MainBox/Main";
-import Introduction from "./components/Details/Introduction";
-import About from "./components/Details/About";
-import Blog from "./components/Details/Blog";
-import Projects from "./components/Details/Projects";
+import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <Main />
+        <Footer />
       </div>
     </Router>
-  );
+  )
+
 }
 
 export default App;
